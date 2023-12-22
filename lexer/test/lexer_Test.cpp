c18ac4b731d42759792a3c1afe5b1bc3fc1de016
Mitesh Kumar <miteshkumarca@gmail.com>
2023-12-22 09:24:23 -0800

    include ctpg and add some tests

--- lexer/test/lexer_Test.cpp
+++ lexer/test/lexer_Test.cpp
@@ -5,8 +5,6 @@
 
 #include <iostream>
 
-TEST(LexerTest, TokenizerTest) {
-    constexpr auto pattern = lexer::tokens::identifier.get_data();
-    constexpr ctpg::regex::expr<pattern.pattern> r;
-    ASSERT_TRUE(r.match(ctpg::match_options{}.set_verbose(), ctpg::buffers::string_buffer("abc123"), std::cout));
+TEST(LexerTest, IdentifierRegexTest)
+{
 }
